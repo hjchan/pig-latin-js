c683811b4df8c03fc21fb6bee2557d3b46c728be
HauJie <haujiechan@gmail.com>
2017-08-15 12:37:39 +0800

    done

--- pig-latin.js
+++ pig-latin.js
@@ -2,9 +2,18 @@ function getPigLatin() {
 
   //get the word input by the user
   var str = document.getElementById("word").value;
-  
+  var arr = str.split(' ')
+  var numOfWord = arr.length
 
-  //your code goes here
+  for (i=0; i<numOfWord; i++){
+  	if (/[aeiou]/.test(arr[i][0])){
+  		arr[i] = arr[i]
+  	} else{
+  		wordLocation = arr[i].search(/[aeiou]/)
+  		arr[i] = arr[i].substring(wordLocation) + arr[i].substring(0, wordLocation) + "ay";
+  	}
+  }
+  str = arr.join(' ');
 
   //change the HTML content of a <p> element with id="pig-latin-word": 
   document.getElementById("pig-latin-word").innerHTML = str;
@@ -14,3 +23,4 @@ function getPigLatin() {
 document.addEventListener('DOMContentLoaded', function(){
     document.getElementById('event-btn').addEventListener("click", getPigLatin);
 });
+
